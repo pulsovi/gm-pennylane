479855940a09d38cce0b0e11051d20c11d4dc777
David GABISON <david.gabison@outlook.com>
2024-11-05 14:35:12 +0100

    fix: set per_page as infinity

--- src/api/operation.ts
+++ src/api/operation.ts
@@ -2,12 +2,12 @@ import { apiRequest } from "./core";
 import { GroupedDocument, LedgerEvent } from "./types";
 
 export async function getLedgerEvents (id): Promise<LedgerEvent[]> {
-  const response = await apiRequest(`accountants/operations/${id}/ledger_events`, null, 'GET');
+  const response = await apiRequest(`accountants/operations/${id}/ledger_events?per_page=-1`, null, 'GET');
   return await response!.json();
 }
 Object.assign(window, {getLedgerEvents});
 
 export async function getGroupedDocuments (id): Promise<GroupedDocument[]> {
-  const response = await apiRequest(`accountants/operations/${id}/grouped_documents`, null, 'GET');
+  const response = await apiRequest(`accountants/operations/${id}/grouped_documents?per_page=-1`, null, 'GET');
   return await response!.json();
 }
