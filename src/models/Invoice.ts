2484c675296f8661d8963bb1cdec18ace5bad99b
David GABISON <david.gabison@outlook.com>
2024-11-05 14:41:57 +0100

    refactor: update valid conditions

--- src/models/Invoice.ts
+++ src/models/Invoice.ts
@@ -1,16 +1,12 @@
 import { getParam } from '../_';
+import { archiveDocument } from '../api/document.js';
 import { getInvoice, updateInvoice } from '../api/invoice.js';
 import { RawInvoice, RawInvoiceUpdate } from '../api/types.js';
 import ValidableDocument from './ValidableDocument.js';
 
 export default abstract class Invoice extends ValidableDocument {
   public readonly type = 'invoice';
-  public invoice: RawInvoice | Promise<RawInvoice>;
-
-  protected constructor (invoice: RawInvoice) {
-    super(invoice);
-    this.getInvoice();
-  }
+  private invoice: RawInvoice | Promise<RawInvoice>;
 
   static from (invoice: RawInvoice) {
     if (invoice.direction === 'supplier') return new SupplierInvoice(invoice);
@@ -55,25 +51,28 @@ class SupplierInvoice extends Invoice {
     if (invoice.id === current)
       console.log('SupplierInvoice.loadValidMessage', { invoice, invoiceDocument });
 
-    // Transaction 2024 => OK
+    // Transaction < 2024 => OK
     const groupedDocuments = await this.getGroupedDocuments();
     const transactions = groupedDocuments.filter(doc => doc.type === 'Transaction');
-    if (transactions.length && transactions.every(transaction => transaction.date.startsWith('2024'))) {
-      if (invoice.id == current) console.log('transactions 2024');
+    const currentYear = 2024; //new Date().getFullYear();
+    if (
+      transactions.length &&
+      transactions.every(transaction => parseInt(transaction.date.slice(0, 4)) < currentYear)
+    ) {
+      if (invoice.id == current) console.log(this.constructor.name, 'loadValidMessage', 'année passée');
       return 'OK';
     }
 
-    if (invoice.thirdparty && !invoice.thirdparty.country)
-      return 'Fournisseur inconnu : Choisir un autre fournisseur ou ajouter le pays de celui-ci';
-
     // Archived
     if (invoice.archived) {
-      return 'OK';
       const allowed = ['§ #', '§ CARTE ETRANGERE', '¤ TRANSACTION INTROUVABLE'];
       if (!allowed.some(allowedItem => invoice.invoice_number.startsWith(allowedItem)))
         return `Le numéro de facture d'une facture archivée doit commencer par une de ces possibilités : ${allowed.map(it => `"${it}"`).join(', ')}`;
       return 'OK';
     }
+
+    if (invoice.thirdparty && !invoice.thirdparty.country)
+      return 'Fournisseur inconnu : Choisir un autre fournisseur ou ajouter le pays de celui-ci';
 
     // Pas de tiers
     if (!invoice.thirdparty_id && !invoice.thirdparty) return 'Ajouter un fournisseur';
@@ -88,10 +87,18 @@ class SupplierInvoice extends Invoice {
       return 'OK';
     }
 
-    // Aides octroyées avec date
-    if ([106438171, 114270419].includes(invoice.thirdparty?.id ?? 0)) {
+    // Aides octroyées ou piece d'indentité avec date
+    if ([106438171, 114270419, 106519227].includes(invoice.thirdparty?.id ?? 0)) {
       if (invoice.date || invoice.deadline) return 'Les dates doivent être vides';
     }
+
+    // Aides octroyées avec mauvais ID
+    if (invoice.thirdparty?.name === "AIDES OCTROYÉES" && invoice.thirdparty.id !== 106438171)
+      return 'Il ne doit y avoir qu\'un seul compte "AIDES OCTROYÉES", et ce n\'est pas le bon...';
+
+    // Piece d'identité avec mauvais ID
+    if (invoice.thirdparty?.name === "PIECE ID" && invoice.thirdparty.id !== 106519227)
+      return 'Il ne doit y avoir qu\'un seul compte "PIECE ID", et ce n\'est pas le bon...';
 
     /* Aides octroyées sans label
     if ([106438171, 114270419].includes(invoice.thirdparty?.id)) {
@@ -118,7 +125,10 @@ class SupplierInvoice extends Invoice {
     if (invoice.thirdparty?.id === 115640202) return 'OK';
 
     // ID card
-    if (invoice.thirdparty?.id === 106519227 && invoice.invoice_number?.startsWith('ID ')) return 'OK';
+    if (invoice.thirdparty?.id === 106519227) {
+      if (invoice.invoice_number?.startsWith('ID ')) return 'OK';
+      else return 'Le "Numéro de facture" des pièces d\'identité commence obligatoirement par "ID "';
+    }
 
     // Has transaction attached
     if (!transactions.length) return 'pas de transaction attachée';
@@ -131,19 +141,42 @@ class CustomerInvoice extends Invoice {
   public readonly direction = 'customer';
 
   async loadValidMessage () {
-    if (!this.invoice.thirdparty) return 'choisir un "client"';
-    if (this.invoice.date || this.invoice.deadline) return 'les dates doivent être vides';
+    const invoice = await this.getInvoice();
+    console.log(this.constructor.name, 'loadValidMessage', { invoice });
+    // Archived
+    if (invoice.archived) {
+      const allowed = ['§ #', '§ ESPECES', '¤ TRANSACTION INTROUVABLE'];
+      if (!allowed.some(allowedItem => invoice.invoice_number.startsWith(allowedItem)))
+        return `Le numéro de facture d'une facture archivée doit commencer par une de ces possibilités : ${allowed.map(it => `"${it}"`).join(', ')}`;
+      return 'OK';
+    }
 
-    // don manuel
-    if (this.invoice.thirdparty.id === 103165930) return 'OK';
+    // Pas de client
+    if (!invoice.thirdparty) return 'choisir un "client"';
+
+    // Les dates doivent toujours être vides
+    if (invoice.date || invoice.deadline) return 'les dates doivent être vides';
 
     // piece id
-    if (this.invoice.thirdparty.id === 113420582) {
-      if (!this.invoice.invoice_number?.startsWith('ID '))
+    if (invoice.thirdparty.id === 113420582) {
+      if (!invoice.invoice_number?.startsWith('ID '))
         return 'le champ "Numéro de facture" doit commencer par "ID NOM_DE_LA_PERSONNE"';
       return 'OK';
     }
 
-    return 'les seuls clients autorisés sont "PIECE ID" et "DON MANUEL"';
+    // don manuel
+    if (![113420582, 103165930].includes(invoice.thirdparty.id))
+      return 'les seuls clients autorisés sont "PIECE ID" et "DON MANUEL"';
+
+    // Has transaction attached
+    const invoiceDocument = await this.getDocument();
+    const groupedDocuments = invoiceDocument.grouped_documents;
+    if (
+      !groupedDocuments?.some(doc => doc.type === 'Transaction')
+      && !invoice.invoice_number.startsWith('¤ ')
+    )
+      return 'pas de transaction attachée';
+
+    return 'OK';
   }
 }
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,45 +1,39 @@
-import type { RawTransaction } from '../api/types.d.ts';
+import type { RawTransactionMin } from '../api/types.d.ts';
 import { getParam } from '../_';
 
 import { documentMatching } from '../api/document.ts';
 import ValidableDocument from './ValidableDocument.ts';
+import { getTransaction } from '../api/transaction.ts';
 
 export default class Transaction extends ValidableDocument {
-  protected transaction: RawTransaction;
+  protected transactionMin: RawTransactionMin;
 
-  constructor(transactionOrId: RawTransaction | number) {
+  constructor(transactionOrId: RawTransactionMin | number) {
     const id = 'number' === typeof transactionOrId ? transactionOrId : transactionOrId.id;
     super({id});
-    if ('object' === typeof transactionOrId) this.transaction = transactionOrId;
+    if ('object' === typeof transactionOrId) this.transactionMin = transactionOrId;
   }
 
   protected async loadValidMessage () {
-    if (this.id === Number(getParam(location.href, 'transaction_id')))
-      console.log('Transaction getValidMessage', this);
+    const isCurrent = this.id === Number(getParam(location.href, 'transaction_id'));
+    if (isCurrent) console.log('Transaction getValidMessage', this);
 
     const doc = await this.getDocument();
 
-    // justificatif demandé
-    if (doc.is_waiting_details) return 'OK';
+    if (
+      doc.label.toUpperCase().startsWith('VIR ')
+      && !doc.label.includes(' DE: STRIPE MOTIF: STRIPE REF: STRIPE-')
+    ) {
+      return 'Virement reçu sans justificatif';
+    }
 
     // N'afficher que les transaction avant 2024
-    if (doc.date.startsWith('2024')) return 'OK';
+    //if (doc.date.startsWith('2024')) return 'OK';
+
+    // Transaction archivée
+    if (doc.archived) return 'OK';
 
     const ledgerEvents = await this.getLedgerEvents();
-
-    //if (doc.grouped_documents.length < 2) return 'justificatif manquant';
-
-    if (ledgerEvents.find(line => line.planItem.number === '6288'))
-      return 'Une ligne d\'écriture comporte le numéro de compte 6288';
-
-    // balance déséquilibrée
-    const third = ledgerEvents.find(line => line.planItem.number.startsWith('40'))?.planItem?.number;
-    if (third) {
-      const balance = ledgerEvents.reduce((sum, line) => {
-        return sum + (line.planItem.number == third ? parseFloat(line.amount) : 0);
-      }, 0);
-      if (balance !== 0) return 'Balance déséquilibrée.';
-    }
 
     // Aides octroyées sans label
     if(ledgerEvents.some(line => line.planItem.number.startsWith('6571') && !line.label))
@@ -48,11 +42,51 @@ export default class Transaction extends ValidableDocument {
     if (ledgerEvents.some(line => line.planItem.number.startsWith('445')))
       return 'Une écriture comporte un compte de TVA';
 
-    if (ledgerEvents.find(line => line.planItem.number === '4716001'))
-      return "Une ligne d'écriture utilise un compte d'attente 4716001";
+    // Pas de rapprochement bancaire
+    const recent = (Date.now() - new Date(doc.date).getTime()) < 86_400_000 * 30;
+    if (!recent && ledgerEvents.some(line => line.planItem.number.startsWith('512') && !line.reconciliation_id))
+      return 'Cette transaction n\'est pas rattachée à un rapprochement bancaire';
 
-    if (ledgerEvents.some(line => line.planItem.number.startsWith('41')))
-      return 'Une écriture comporte un compte d\'attente';
+    // justificatif demandé
+    if (!doc.is_waiting_details || isCurrent) {
+      if (ledgerEvents.find(line => line.planItem.number === '6288'))
+        return 'Une ligne d\'écriture comporte le numéro de compte 6288';
+
+      if (ledgerEvents.find(line => line.planItem.number === '4716001'))
+        return "Une ligne d'écriture utilise un compte d'attente 4716001";
+
+      if (ledgerEvents.some(line => line.planItem.number.startsWith('41')))
+        return 'Une écriture comporte un compte d\'attente';
+
+      // balance déséquilibrée
+      const third = ledgerEvents.find(line => line.planItem.number.startsWith('40'))?.planItem?.number;
+      if (third) {
+        const thirdEvents = ledgerEvents.filter(line => line.planItem.number === third);
+        const balance = thirdEvents.reduce((sum, line) => sum + parseFloat(line.amount), 0);
+        if (this.id === Number(getParam(location.href, 'transaction_id')))
+          console.log(this.constructor.name, 'loadValidMessage: Balance', Math.abs(balance) > 0.001 ? 'déséquilibrée' : 'OK', { third, thirdEvents, balance, ledgerEvents, [this.constructor.name]: this });
+        if (Math.abs(balance) > 0.001) {
+          // On a parfois des calculs qui ne tombent pas très juste en JS
+          return `Balance déséquilibrée: ${balance}`;
+        }
+      }
+
+      // Pas plus d'exigence pour les petits montants
+      if (Math.abs(parseFloat(doc.currency_amount)) < 100) return 'OK';
+
+      // Justificatif manquant
+      const attachmentOptional = Math.abs(parseFloat(doc.currency_amount)) < 100 || [
+        'REMISE CHEQUE ',
+        'VIR RECU ',
+        'VIR INST RE ',
+        'VIR INSTANTANE RECU DE: ',
+      ].some(label => doc.label.startsWith(label));
+      const attachmentRequired = doc.attachment_required && !doc.attachment_lost
+        && (!attachmentOptional || isCurrent);
+      const groupedDocuments = await this.getGroupedDocuments();
+      const hasAttachment = groupedDocuments.length > 1;
+      if (attachmentRequired && !hasAttachment) return 'Justificatif manquant';
+    }
 
     return 'OK';
   }
