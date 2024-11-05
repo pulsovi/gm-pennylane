e265f9cf253798a2d53898d1334bf7ca563b1541
David GABISON <david.gabison@outlook.com>
2024-11-05 14:34:57 +0100

    feat: add types

--- src/api/operation.ts
+++ src/api/operation.ts
@@ -1,12 +1,13 @@
 import { apiRequest } from "./core";
+import { GroupedDocument, LedgerEvent } from "./types";
 
-export async function getLedgerEvents (id) {
+export async function getLedgerEvents (id): Promise<LedgerEvent[]> {
   const response = await apiRequest(`accountants/operations/${id}/ledger_events`, null, 'GET');
   return await response!.json();
 }
-window.getLedgerEvents = getLedgerEvents;
+Object.assign(window, {getLedgerEvents});
 
-export async function getGroupedDocuments (id) {
+export async function getGroupedDocuments (id): Promise<GroupedDocument[]> {
   const response = await apiRequest(`accountants/operations/${id}/grouped_documents`, null, 'GET');
   return await response!.json();
 }
