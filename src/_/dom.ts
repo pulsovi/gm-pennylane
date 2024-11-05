7ba84bce2848a7cb3418d4b09d17b55d2cf690f2
David GABISON <david.gabison@outlook.com>
2024-11-05 14:33:23 +0100

    feat: add root arg for $ and $$ util functions

--- src/_/dom.ts
+++ src/_/dom.ts
@@ -1,11 +1,11 @@
 import { waitFunc } from './time.js';
 
-export function $ <T extends Element>(selector: string) {
-  return document.querySelector<T>(selector);
+export function $ <T extends Element>(selector: string, root: ParentNode = document) {
+  return root.querySelector<T>(selector);
 }
 
-export function $$ <T extends Element>(selector: string) {
-  return Array.from(document.querySelectorAll<T>(selector));
+export function $$ <T extends Element>(selector: string, root: ParentNode = document) {
+  return Array.from(root.querySelectorAll<T>(selector));
 }
 
 export async function waitElem <T extends Element>(selector: string, content?: string) {
@@ -15,14 +15,12 @@ export async function waitElem <T extends Element>(selector: string, content?: s
 export function findElem <T extends Element>(selector: string, content?: string) {
   return $$<T>(selector).find(el => !content || el.textContent === content) ?? null;
 }
-window.findElem = findElem;
 
 export function parentElement (child, steps = 1) {
   let parent = child;
   for (let i = 0; i < steps; ++i) parent = parent?.parentElement;
   return parent;
 }
-window.parentElement = parentElement;
 
 export function upElement (elem, upCount) {
   let retval = elem;
@@ -42,3 +40,13 @@ export function parseHTML(html) {
     template.innerHTML = html;
     return template.content;
 }
+
+Object.assign(window, { gm: {
+  $$,
+  $,
+  findElem,
+  parentElement,
+  parseHTML,
+  upElement,
+  waitElem,
+}})
