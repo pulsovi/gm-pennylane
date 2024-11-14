948b02c82653a05aa003b6c6bc1397d714c5883a
David GABISON <david.gabison@outlook.com>
2024-11-14 23:12:42 +0100

    chore: up version

--- pennylane.meta.js
+++ pennylane.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Pennylane
-// @version  0.1.6
+// @version  0.1.7
 // @grant    unsafeWindow
 // @grant    GM.openInTab
 // @match    https://app.pennylane.com/companies/*
