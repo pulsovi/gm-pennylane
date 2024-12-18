67467790c55a2011e88979d0a92d95baf4db8812
David GABISON <david.gabison@outlook.com>
2024-12-18 22:11:38 +0100

    refactor: add log on update current element status

--- src/framework/CacheListRecord.ts
+++ src/framework/CacheListRecord.ts
@@ -1,3 +1,4 @@
+import { getParam } from "../_";
 import CacheList from "./CacheList";
 
 export default class CacheListRecord<T extends object> extends CacheList<T> {
@@ -13,6 +14,8 @@ export default class CacheListRecord<T extends object> extends CacheList<T> {
     if (oldValue) {
       newValue = {...oldValue, ...newValue};
       this.data.splice(this.data.indexOf(oldValue), 1, newValue);
+      if (newValue.id == getParam(location.href, 'id'))
+        this.log('updateItem', { match, create, oldValue, newValue, stack: new Error('').stack });
       this.emit('update', { oldValue, newValue });
     } else {
       if (!create) return;
