0f7b215f6dc606a43e63d234ced87e1fd3604cbe
David GABISON <david.gabison@outlook.com>
2025-04-29 10:25:27 +0200

    refactor: avoid assume dms folder

--- src/pages/transactionPanel/ArchiveGroupedDocument.ts
+++ src/pages/transactionPanel/ArchiveGroupedDocument.ts
@@ -29,6 +29,10 @@ export default class ArchiveGroupedDocument extends Service {
 
     buttonsBlock.insertBefore(
       parseHTML(`
+        `/*<button
+          class="dms-button noCaret ui-button ui-button-sm ui-button-secondary ui-button-secondary-primary ui-button-sm-icon-only" aria-haspopup="true" aria-expanded="false" type="button">
+          <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mr-0 css-q7mezt" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DriveFileMoveRoundedIcon" style="font-size: 1rem;"><path d="M20 6h-8l-1.41-1.41C10.21 4.21 9.7 4 9.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2m-8 9.79V14H9c-.55 0-1-.45-1-1s.45-1 1-1h3v-1.79c0-.45.54-.67.85-.35l2.79 2.79c.2.2.2.51 0 .71l-2.79 2.79c-.31.31-.85.09-.85-.36"></path></svg>
+        </button>*/+`
         <button class="archive-button ${buttonClass}">&nbsp;x&nbsp;</button>
       `),
       buttonsBlock.firstElementChild
@@ -57,6 +61,25 @@ export default class ArchiveGroupedDocument extends Service {
       this.log(`archive invoice #${id}`, {invoice});
       ValidMessage.getInstance().reload();
     });
+
+    /*
+    const dmsButton = buttonsBlock.querySelector<HTMLButtonElement>('.dms-button');
+    dmsButton!.addEventListener('click', async () => {
+      dmsButton.disabled = true;
+      dmsButton.classList.add('disabled');
+      dmsButton.innerText = '⟳';
+      const invoice = await Invoice.load(id);
+      if (!invoice) {
+        alert('Impossible de trouver la facture #'+id);
+        dmsButton.innerText = '⚠';
+        return;
+      }
+      await moveToDms(
+        invoice.id,
+        84899227 // 2024/justificatifs Achats
+      );
+    });
+    */
 
     upElement(buttonsBlock, 3).querySelector('.flex-grow-1 .d-block:last-child')?.appendChild(
       parseHTML(
