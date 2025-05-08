0256ceb130be07d96b8d115c1f515d296f1495ba
David GABISON <david.gabison@outlook.com>
2025-05-08 15:49:28 +0200

    fix: "#undefined"

--- src/pages/transactionPanel/ArchiveGroupedDocument.ts
+++ src/pages/transactionPanel/ArchiveGroupedDocument.ts
@@ -7,7 +7,7 @@ import ValidMessage from './ValidMessage.js';
 
 /** Add "Archive" button on bonded invoice in transaction pannel */
 export default class ArchiveGroupedDocument extends Service {
-  async init () {
+  async init() {
     await waitElem('h3', 'Transactions');
 
     while (await waitFunc(() =>
@@ -15,7 +15,7 @@ export default class ArchiveGroupedDocument extends Service {
     )) this.addInvoiceInfos();
   }
 
-  addInvoiceInfos () {
+  addInvoiceInfos() {
     const buttonsBlock = $$('div>a+button>svg')
       .find(svg => !svg.closest('div')?.querySelector('.archive-button'))
       ?.closest('div');
@@ -32,7 +32,7 @@ export default class ArchiveGroupedDocument extends Service {
         `/*<button
           class="dms-button noCaret ui-button ui-button-sm ui-button-secondary ui-button-secondary-primary ui-button-sm-icon-only" aria-haspopup="true" aria-expanded="false" type="button">
           <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium mr-0 css-q7mezt" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DriveFileMoveRoundedIcon" style="font-size: 1rem;"><path d="M20 6h-8l-1.41-1.41C10.21 4.21 9.7 4 9.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2m-8 9.79V14H9c-.55 0-1-.45-1-1s.45-1 1-1h3v-1.79c0-.45.54-.67.85-.35l2.79 2.79c.2.2.2.51 0 .71l-2.79 2.79c-.31.31-.85.09-.85-.36"></path></svg>
-        </button>*/+`
+        </button>*/+ `
         <button class="archive-button ${buttonClass}">&nbsp;x&nbsp;</button>
       `),
       buttonsBlock.firstElementChild
@@ -46,7 +46,7 @@ export default class ArchiveGroupedDocument extends Service {
       archiveButton.innerText = '⟳';
       const invoice = await Invoice.load(id);
       if (!invoice) {
-        alert('Impossible de trouver la facture #'+id);
+        alert('Impossible de trouver la facture #' + id);
         archiveButton.innerText = '⚠';
         return;
       }
@@ -58,7 +58,7 @@ export default class ArchiveGroupedDocument extends Service {
       });
       await invoice.archive();
       buttonsBlock.closest('.ui-card')?.remove();
-      this.log(`archive invoice #${id}`, {invoice});
+      this.log(`archive invoice #${id}`, { invoice });
       ValidMessage.getInstance().reload();
     });
 
@@ -81,10 +81,15 @@ export default class ArchiveGroupedDocument extends Service {
     });
     */
 
-    upElement(buttonsBlock, 3).querySelector('.flex-grow-1 .d-block:last-child')?.appendChild(
-      parseHTML(
-        `&nbsp;<span class="invoice-id d-inline-block bg-secondary-100 dihsuQ px-0_5">#${id}</span>`
-      )
-    );
+    if (id) {
+      this.log({buttonsBlock, id});
+      upElement(buttonsBlock, 3).querySelector('.flex-grow-1 .d-block:last-child')?.appendChild(
+        parseHTML(
+          `&nbsp;<span class="invoice-id d-inline-block bg-secondary-100 dihsuQ px-0_5">#${id}</span>`
+        )
+      );
+    } else {
+      this.log('Impossible de retrouver l\'ID', { buttonsBlock });
+    }
   }
 }
