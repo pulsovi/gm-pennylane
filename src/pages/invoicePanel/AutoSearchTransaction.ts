471b9baa6d47b120dfa551eda2697686e0a8f4fa
David GABISON <david.gabison@outlook.com>
2025-05-26 13:15:01 +0200

    feat: add more criteria for auto search invoice transaction

--- src/pages/invoicePanel/AutoSearchTransaction.ts
+++ src/pages/invoicePanel/AutoSearchTransaction.ts
@@ -23,12 +23,45 @@ export default class AutoSearchTransaction extends Service {
     Tooltip.make({ target: this.container, text: 'Chercher une transaction de ce montant' });
     this.container.addEventListener('click', event => {
       const amountInput = findElem<HTMLInputElement>('input[name="currency_amount"]')!;
-      const urlRoot = location.href.split('/').slice(0, 6).join('/');
-      const url = `${urlRoot}/transactions?filter=${JSON.stringify([{
+      const filters = [{
         field: "amount",
         operator: "abs_eq",
-        value: parseFloat(amountInput.value.replace(',', '.').replace(/ /gu, '')),
-      }])}&per_page=300&sort=-date&sidepanel_tab=reconciliation`;
+        value: amountInput.value.replace(',', '.').replace(/ /gu, ''),
+      }];
+
+      const [minDate, maxDate] = [
+        $<HTMLInputElement>('input[name="date"]')!,
+        $<HTMLInputElement>('input[name="deadline"]')!,
+      ].map((input, id) => {
+        if (!input.value) return id ? '2025-12-31' : '2024-01-01';
+        return new Date(input.value.split('/').reverse().join('-')).toISOString();
+      });
+      filters.push(
+        {
+          field: "date",
+          operator: "gteq",
+          value: minDate,
+        }, {
+        field: "date",
+        operator: "lteq",
+        value: maxDate,
+      }
+      );
+
+      const match = $<HTMLInputElement>('input[name="invoice_number"')!.value
+        .match(/CHQ ?(?:n° ?)?(?<chq_number>\d+)/u)?.groups;
+      if (match) {
+        filters.push({
+          field: "label",
+          operator: "search",
+          value: `CHEQUE ${match.chq_number}`,
+        });
+      }
+
+      const urlRoot = location.href.split('/').slice(0, 6).join('/');
+      const url = `${urlRoot}/transactions?filter=${JSON.stringify(filters)
+        }&per_page=300&sort=-date&sidepanel_tab=reconciliation&period_start=${minDate.slice(0, 4)
+        }-01-01&period_end=${maxDate.slice(0, 4)}-12-31`;
       openInTab(url);
     });
   }
