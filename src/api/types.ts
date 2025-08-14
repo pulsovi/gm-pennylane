5112b7c9e39af2ae2995cdb416bdac6cadb5c8bc
David GABISON <david.gabison@outlook.com>
2025-08-14 08:47:16 +0200

    fix: types

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,9 +2,6 @@ import { APIDocument } from "./Document/index.js";
 import { APIGroupedDocument } from "./GroupedDocument/index.js";
 import { APIInvoiceList } from "./Invoice/List.js";
 
-type CommonKeys<T, U> = keyof T & keyof U;
-type CommonProperties<T, U> = Pick<T, CommonKeys<T, U>>;
-
 export type APIInvoiceItem = APIInvoiceList['invoices'][number];
 export type Direction = 'customer'|'supplier';
-export type GroupedDocument = CommonProperties<APIGroupedDocument, APIDocument['grouped_documents'][number]>;
+export type GroupedDocument = APIGroupedDocument | APIDocument['grouped_documents'][number];
