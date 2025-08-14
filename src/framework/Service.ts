4a8ffed7e262201290235e13c5d624ee67ac5439
David GABISON <david.gabison@outlook.com>
2025-08-14 09:01:05 +0200

    chore: format

--- src/framework/Service.ts
+++ src/framework/Service.ts
@@ -3,20 +3,23 @@ import Logger from './Logger.js';
 export default class Service extends Logger {
   protected static instance: Service;
 
-  protected constructor () {
+  protected constructor() {
     super();
     this.init();
   }
 
-  public static start () {
+  public static start() {
     console.log(this.name, 'start', this);
     this.getInstance();
   }
 
-  public static getInstance () {
-    if (!this.instance) this.instance = new this();
+  public static getInstance() {
+    if (!this.instance) {
+      const instance = new this();
+      this.instance = instance;
+    }
     return this.instance;
   }
 
-  protected init (): void | Promise<void> {};
+  protected init(): void | Promise<void> {};
 }
