58c2349e5073d1614e3a7ad8abeaf6415f7241d8
David GABISON <david.gabison@outlook.com>
2025-09-15 14:35:12 +0200

    feat: add loading spinner and hotkey tooltip

--- src/pages/transactionPanel/AddByIdButton.ts
+++ src/pages/transactionPanel/AddByIdButton.ts
@@ -1,67 +1,88 @@
 import { $, getParam, parseHTML, waitElem, waitFunc } from '../../_/index.js';
 import Service from '../../framework/Service.js';
+import Tooltip from "../../framework/Tooltip.js";
 import Transaction from '../../models/Transaction.js';
 import { isPage } from '../../navigation/waitPage.js';
 import ValidMessage from './ValidMessage.js';
 
 /** Add 'add by ID' button on transaction reconciliation tab */
 export default class TransactionAddByIdButton extends Service {
-  private button = parseHTML('<div class="btn-sm w-100 btn-primary add-by-id-btn" style="cursor: pointer;">Ajouter par ID</div>').firstElementChild as HTMLDivElement;
+  private button = parseHTML(
+    '<div class="btn-sm w-100 btn-primary add-by-id-btn" style="cursor: pointer;">Ajouter par ID</div>'
+  ).firstElementChild as HTMLDivElement;
+  private disabled = false;
 
-  async init () {
+  async init() {
     await this.insertContainer();
     this.attachEvent();
   }
 
-  async insertContainer () {
-    const div = (await Promise.race([
-      waitElem('button', 'Voir plus de factures'),
-      waitElem('button', 'Chercher parmi les factures'),
-    ])).closest<HTMLDivElement>('.mt-2');
+  async insertContainer() {
+    const div = (
+      await Promise.race([
+        waitElem("button", "Voir plus de factures"),
+        waitElem("button", "Chercher parmi les factures"),
+      ])
+    ).closest<HTMLDivElement>(".mt-2");
 
     if (!div) {
-      this.log('TransactionAddByIdButton', { button: await Promise.race([
-        waitElem('button', 'Voir plus de factures'),
-        waitElem('button', 'Chercher parmi les factures'),
-      ]), div });
-      throw new Error('Impossible de trouver le bloc de boutons');
+      this.log("TransactionAddByIdButton", {
+        button: await Promise.race([
+          waitElem("button", "Voir plus de factures"),
+          waitElem("button", "Chercher parmi les factures"),
+        ]),
+        div,
+      });
+      throw new Error("Impossible de trouver le bloc de boutons");
     }
 
     div.insertBefore(this.button, div.lastElementChild);
+    Tooltip.make({ target: this.button, text: "Ajouter par ID (Alt+Z)" });
 
     waitFunc(async () => {
-      const currentDiv = (await Promise.race([
-        waitElem('button', 'Voir plus de factures'),
-        waitElem('button', 'Chercher parmi les factures'),
-      ])).closest<HTMLDivElement>('.mt-2')
+      const currentDiv = (
+        await Promise.race([
+          waitElem("button", "Voir plus de factures"),
+          waitElem("button", "Chercher parmi les factures"),
+        ])
+      ).closest<HTMLDivElement>(".mt-2");
       return currentDiv !== div;
     }).then(() => this.insertContainer());
   }
 
-  attachEvent () {
+  attachEvent() {
     this.log({ button: this.button });
-    this.button.addEventListener('click', () => { this.addById(); });
-    document.addEventListener('keydown', event => {
-      if (isPage('transactionDetail') && event.altKey && ['z', 'Z'].includes(event.key)) {
+    this.button.addEventListener("click", () => {
+      this.addById();
+    });
+    document.addEventListener("keydown", (event) => {
+      if (isPage("transactionDetail") && event.altKey && ["z", "Z"].includes(event.key)) {
         this.addById();
       } else {
         this.debug({
-          isPageTransactionDetail: isPage('transactionDetail'),
+          isPageTransactionDetail: isPage("transactionDetail"),
           event,
         });
       }
     });
   }
 
-  async addById () {
+  async addById() {
+    if (this.disabled) return;
     /**
      * Obligé de recharger la transaction à chaque appel : le numéro guid du
      * groupe change à chaque attachement de nouvelle pièce
      */
-    const transactionId = Number(getParam(location.href, 'transaction_id'));
-    const id = Number(prompt('ID du justificatif ?'));
+    const transactionId = Number(getParam(location.href, "transaction_id"));
+    const id = Number(prompt("ID du justificatif ?"));
     const transaction = new Transaction({ id: transactionId });
+    this.disabled = true;
+    const spinner = parseHTML('<div class="spinner-border spinner-border-sm" role="status"></div>')
+      .firstElementChild as HTMLDivElement;
+    this.button.appendChild(spinner);
     await transaction.groupAdd(id);
+    spinner.remove();
+    this.disabled = false;
     ValidMessage.getInstance().reload();
   }
 }
