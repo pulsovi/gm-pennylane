7ef08c25ac19ae4f60a72c75682418733eab7a49
David GABISON <david.gabison@outlook.com>
2025-09-25 10:21:01 +0200

    refactor: adapt search text to direction

--- src/pages/invoicePanel/AutoSearchTransaction.ts
+++ src/pages/invoicePanel/AutoSearchTransaction.ts
@@ -1,7 +1,10 @@
 import { $, findElem, parseHTML } from "../../_/dom.js";
 import { getButtonClassName } from "../../_/getButtonClassName.js";
+import { findReactProp, getReactPropValue } from "../../_/react.js";
 import { waitFunc } from "../../_/time.js";
 import { getParam } from "../../_/url.js";
+import { APIInvoice } from "../../api/Invoice/index.js";
+import { InvoicesEntity } from "../../api/Invoice/List.js";
 import Service from "../../framework/Service.js";
 import Tooltip from "../../framework/Tooltip.js";
 import { openInTab } from "../../GM/openInTab.js";
@@ -21,51 +24,66 @@ export default class AutoSearchTransaction extends Service {
       class="${getButtonClassName()} auto-find-transaction-button"
       style="margin-left: 1em; padding: .2em;"
     >🔍</button>`).firstElementChild as HTMLButtonElement;
-    Tooltip.make({ target: this.container, text: 'Chercher une transaction de ce montant' });
-    this.container.addEventListener('click', event => {
+    Tooltip.make({ target: this.container, text: "Chercher une transaction de ce montant" });
+    this.container.addEventListener("click", (event) => {
       const amountInput = findElem<HTMLInputElement>('input[name="currency_amount"]')!;
-      const filters = [{
-        field: "amount",
-        operator: "abs_eq",
-        value: amountInput.value.replace(',', '.').replace(/ /gu, ''),
-      }];
+      const filters = [
+        {
+          field: "amount",
+          operator: "abs_eq",
+          value: amountInput.value.replace(",", ".").replace(/ /gu, ""),
+        },
+      ];
 
       const [minDate, maxDate] = [
         $<HTMLInputElement>('input[name="date"]')!,
         $<HTMLInputElement>('input[name="deadline"]')!,
       ].map((input, id) => {
-        if (!input.value) return id ? '2025-12-31' : '2024-01-01';
-        return new Date(input.value.split('/').reverse().join('-')).toISOString();
+        if (!input.value) return id ? "2025-12-31" : "2024-01-01";
+        return new Date(input.value.split("/").reverse().join("-")).toISOString();
       });
       filters.push(
         {
           field: "date",
           operator: "gteq",
           value: minDate,
-        }, {
-        field: "date",
-        operator: "lteq",
-        value: maxDate,
-      }
+        },
+        {
+          field: "date",
+          operator: "lteq",
+          value: maxDate,
+        }
       );
 
-      const match = $<HTMLInputElement>('input[name="invoice_number"')!.value
-        .match(/CHQ ?(?:n° ?)?(?<chq_number>\d+)/u)?.groups;
+      const invoiceNumber = $<HTMLInputElement>('input[name="invoice_number"')!;
+      const invoice = APIInvoice.Create(getReactPropValue(invoiceNumber, "invoice"));
+      const match = invoiceNumber.value.match(/CHQ ?(?:n° ?)?(?<chq_number>\d+)/u)?.groups;
       if (match) {
-        filters.push({
-          field: "label",
-          operator: "search",
-          value: `CHEQUE ${match.chq_number}`,
-        });
+        if (invoice.direction === "supplier") {
+          filters.push({
+            field: "label",
+            operator: "search",
+            value: `CHEQUE ${match.chq_number}`,
+          });
+        } else {
+          filters.push({
+            field: "label",
+            operator: "search",
+            value: `REMISE CHEQUE `,
+          });
+        }
       }
 
-      const urlRoot = location.href.split('/').slice(0, 6).join('/');
-      const url = `${urlRoot}/transactions?filter=${JSON.stringify(filters)
-        }&per_page=300&sort=-date&sidepanel_tab=reconciliation&period_start=${minDate.slice(0, 4)
-        }-01-01&period_end=${maxDate.slice(0, 4)}-12-31`;
+      const urlRoot = location.href.split("/").slice(0, 6).join("/");
+      const url = `${urlRoot}/transactions?filter=${JSON.stringify(
+        filters
+      )}&per_page=300&sort=-date&sidepanel_tab=reconciliation&period_start=${minDate.slice(
+        0,
+        4
+      )}-01-01&period_end=${maxDate.slice(0, 4)}-12-31`;
 
       openInTab(url);
-      navigator.clipboard.writeText(getParam(location.href, 'id'))
+      navigator.clipboard.writeText(getParam(location.href, "id"));
     });
   }
 
@@ -73,9 +91,9 @@ export default class AutoSearchTransaction extends Service {
     const refEl = await waitPage("invoiceDetail");
 
     const amountInput = findElem<HTMLInputElement>('input[name="currency_amount"]')!;
-    amountInput.closest('.ui-form-group').querySelector('label').appendChild(this.container);
+    amountInput.closest(".ui-form-group").querySelector("label").appendChild(this.container);
 
-    await waitFunc(async () => await waitPage('invoiceDetail') !== refEl);
+    await waitFunc(async () => (await waitPage("invoiceDetail")) !== refEl);
     this.init();
   }
 }
